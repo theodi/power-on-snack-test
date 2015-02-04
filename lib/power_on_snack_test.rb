20b124cbe0b02be0b7013374ec3f5c9df21bf266
pezholio <pezholio@gmail.com>
2015-02-04 10:22:12 +0000

    Show latest headlines

--- lib/power_on_snack_test.rb
+++ lib/power_on_snack_test.rb
@@ -5,6 +5,17 @@ class PowerOnSnackTest < Sinatra::Base
 
   FLAVOURS = VendingMachine.flavours.keys
 
+  get '/' do
+    @headlines = CSV.parse(File.read('config/headlines.csv')).map! { |l|
+      {
+        headline: l[0],
+        url: l[1],
+        triggered: l[2] == 'true'
+      }
+    }
+    erb :index
+  end
+
   post '/dispense' do
     if FLAVOURS.include?(params[:flavour])
       VendingMachine.instance.dispense(params[:flavour])
--- spec/power_on_snack_test_spec.rb
+++ spec/power_on_snack_test_spec.rb
@@ -13,4 +13,14 @@ describe PowerOnSnackTest do
     expect(last_response.status).to eq(203)
   end
 
+  it "displays a list of the latest headlines" do
+    allow(File).to receive(:read).and_return(File.read("./spec/fixtures/headlines.csv"))
+    get '/'
+    expect(last_response.status).to eq(200)
+    expect(last_response.body).to match(/VIDEO: Vatican priests who are also recession astrophysicists/)
+    expect(last_response.body).to match(/http:\/\/www.bbc.co.uk\/news\/magazine-31051635#sa-ns_mchannel=rss&ns_source=PublicRSS20-sa/)
+    expect(last_response.body).to match(/<li class="triggered"><a href="http:\/\/www.bbc.co.uk\/news\/magazine-31051635/)
+    expect(last_response.body).to match(/<li class="normal"><a href="http:\/\/www.bbc.co.uk\/news\/uk-3110437/)
+  end
+
 end
